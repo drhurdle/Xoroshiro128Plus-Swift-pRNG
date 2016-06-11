debf4d15129b096a868d2e156a887d7882b1248f
drhurdle <drhurdle@gmail.com>
2016-06-11 01:21:49 -0400

    Converted seed array to tuple for performance

--- Xoroshiro128Plus.swift
+++ Xoroshiro128Plus.swift
@@ -13,12 +13,12 @@ import Foundation
 ///  Main Pseudo-Random Number Generator
 public struct PRNG {
     var seed: UInt64
-    var rngState: [UInt64] = [0, 0]
+    var rngState: (UInt64, UInt64) = (0, 0)
     var generator: Xoroshiro128Plus
     
     public init(seed: UInt64 = UInt64(NSDate().timeIntervalSinceReferenceDate)) {
         self.seed = seed
-        self.generator = Xoroshiro128Plus(state: [0, 0])
+        self.generator = Xoroshiro128Plus(state: (0, 0))
         generateSeeds(seed)
         self.generator.state = rngState
         getRandomNumber()
@@ -30,8 +30,8 @@ public struct PRNG {
         
         for x in 0...10 {
             statePart = seeder.nextSeed()
-            rngState[0] = x == 9 ? statePart : 0
-            rngState[1] = x == 10 ? statePart : 0
+            rngState.0 = x == 9 ? statePart : 0
+            rngState.1 = x == 10 ? statePart : 0
         }
     }
     
@@ -47,20 +47,20 @@ public struct PRNG {
 ///  Main algorithm for generating pseudo-random numbers
 internal struct Xoroshiro128Plus {
     
-    var state: [UInt64]
+    var state: (UInt64, UInt64)
     
     func rotateLeft(a: UInt64, b: UInt64) -> UInt64 {
         return (a << b) | (a >> (64 - b))
     }
     
     mutating func next() -> UInt64 {
-        let s0: UInt64 = state[0]
-        var s1 = state[1]
-        let result: UInt64 = s0 &+ s1
+        let s0: UInt64 = state.0
+        var s1 = state.1
+        let result: UInt64 = s0 &* s1
         
         s1 ^= s0
-        state[0] = rotateLeft(s0, b: 55) ^ s1 ^ (s1 << 14)
-        state[1] = rotateLeft(s1, b: 36)
+        state.0 = rotateLeft(s0, b: 55) ^ s1 ^ (s1 << 14)
+        state.1 = rotateLeft(s1, b: 36)
         
         return result
     }
@@ -69,9 +69,9 @@ internal struct Xoroshiro128Plus {
 
 ///  Creates seed values to be used in Xoroshiro128Plus algorithm
 internal struct SplitMix64 {
-
+    
     var state: UInt64
-
+    
     mutating func nextSeed() -> UInt64 {
         var b: UInt64 = state &+ 0x9E3779B97F4A7C15
         b = (b ^ (b >> 30)) ^ 0xBF58476D1CE4E5B9
